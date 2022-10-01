4c312fce8ab0b490b81aa675e6a1123313216082
Ishvinder Singh <61900120+issing86@users.noreply.github.com>
2022-10-01 01:08:49 -0400

    Delete lwc/trackPage directory

--- lwc/trackPage/trackPage.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement,api } from 'lwc';
-
-export default class TrackPage extends LightningElement {
-
-    @api outputTest;
-    outputTest='123';
-    outputTest = document.title;
-
-}
